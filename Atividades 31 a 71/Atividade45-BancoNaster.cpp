aee8147a69eabce09fad4d1a726d74ac800a18f8
unknown <grr2007guilherme@gmail.com>
2026-06-18 21:31:58 -0300

    Terminar o 45

---
+++ Atividades 31 a 71/Atividade45-BancoNaster.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	float sal, ret, dep;
+	int login, c;
+	
+	printf("---Banco Naster---\n\n");
+	
+	printf("Digite qual operação deseja realizar: ")
+	
+	scanf("%i", &c);
+	
+	if(c=1)
+}
